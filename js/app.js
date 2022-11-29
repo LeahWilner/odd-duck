548c96f91c1d0489566a2034bc81fb21e5d1e86b
LEAH WILNER <leahwilner1@gmail.com>
2022-11-29 12:41:15 -0600

    changed for loop

--- js/app.js
+++ js/app.js
@@ -8,12 +8,12 @@ console.log('hello js');
 // Times the image has been shown
 
 Product.allProductsArray = [];
-let ProductContainer = document.querySelector('section');
+let productContainer = document.querySelector('section');
 let results = document.querySelector('section + div');
 let pic1 = document.getElementById('pic1');
 let pic2 = document.getElementById('pic2');
 let pic3 = document.getElementById('pic3');
-console.log(ProductContainer, results, pic1, pic2, pic3);
+console.log(productContainer, results, pic1, pic2, pic3);
 
 let clicks = 0;
 let maxClicks =25;
@@ -31,7 +31,7 @@ function Product(name, src){
 console.log('is Product working? ',Product.allProductsArray);
 
 function getRandomNumber(){
-  return Math.floor(Math.random() * Product.allProductsArray.length);
+  return Math.floor(Math.random() * Product.allProductsArray.length+1);
 }
 
 function renderProducts(){
@@ -39,20 +39,20 @@ function renderProducts(){
   let product2 = getRandomNumber();
   let product3 = getRandomNumber();
 
-  if (pic1 === pic2 || pic1 === pic3){
-    pic1 = getRandomNumber();
+  if (product1 === product2 || product1 === product3){
+    product1 = getRandomNumber();
   }
 
-  if (pic2 === pic3 || pic2 === pic1){
-    pic2 = getRandomNumber();
+  if (product2 === product3 || product2 === product1){
+    product2 = getRandomNumber();
   }
-  if(pic3 === pic2 || pic3 === pic1){
-    pic3 = getRandomNumber();
+  if(product3 === product2 || product3 === product1){
+    product3 = getRandomNumber();
   }
-  
+
   console.log(product1, product2, product3);
 
-  
+
 
   pic1.src = Product.allProductsArray[product1].src;
   pic2.src = Product.allProductsArray[product2].src;
@@ -71,34 +71,34 @@ function handleProductClick(event){
   console.log('are we clicking? ', event);
   if(event.target === productContainer){
     alert('Please click on a product');
-  }  
+  }
   clicks++;
   let clickProduct = event.target.alt;
   console.log(clickProduct);
-  for(let i = 0; 1 < Product.allProductsArray.length; 1++){
+  for(let i = 0; i < Product.allProductsArray.length; i++){
     if(clickProduct === Product.allProductsArray[i].name){
-        Product.allProductsArray[i].click++;
-        break
+      Product.allProductsArray[i].click++;
+      break;
     }
   }
 }
 
 if(clicks === maxClicks){
-    productContainer.removeEventListener('click', handleProductClick);
-    results.addEventListener('click', renderResults);
-    productContainer.className = 'no voting';
-}   else{
-    renderProducts();
+  productContainer.removeEventListener('click', handleProductClick);
+  results.addEventListener('click', renderResults);
+  productContainer.className = 'no voting';
+} else{
+  renderProducts();
 }
 
 
 function renderResults(){
-    let ul = document.getElementById('ul');
-    for(let i =0; i < Product.allProductsArray.length; i++){
-        let li = document.createElement('li');
-        li.textContent = `${Product.allProductsArray[i].name} had ${Product.allProductsArray[i].views} and were clicked on ${Product.allProductsArray[i].click} times`;
-        ul.appendChild(li);
-    }
+  let ul = document.getElementById('ul');
+  for(let i =0; i < Product.allProductsArray.length; i++){
+    let li = document.createElement('li');
+    li.textContent = `${Product.allProductsArray[i].name} had ${Product.allProductsArray[i].views} and were clicked on ${Product.allProductsArray[i].click} times`;
+    ul.appendChild(li);
+  }
 }
 
 new Product('bag', 'images/bag.jpg');
@@ -110,9 +110,9 @@ new Product('bubblegum', 'images/bubblegum.jpg');
 new Product('chair', 'images/chair.jpg');
 new Product('cthulhu', 'images/cthulhu.jpg');
 new Product('dog-duck', 'images/dog-duck.jpg');
-new Product
-new Product
-new Product
+new Product;
+new Product;
+new Product;
 
 
 
